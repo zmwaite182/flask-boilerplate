aab3704e7cc4fb3e826069935a997b27463b5197
Zane Waite <zmwaite182@stevenscollege.edu>
2019-03-20 15:01:52 -0400

    Add more routes and tests
    -Import request
    -Add /hello and /number routes
    -Add hello and number test functions

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -14,6 +14,19 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        return 'Hello World'
+        return 'This is a flask boilerplate project, not to be used in production'
+
+    @app.route('/hello')
+    def hello():
+
+        for key, value in request.args.items():
+            print(f"{key}: {value}")
+
+        name = request.args.get('name', 'World')
+        return f'Hello {name}'
+
+    @app.route('/number/<int:n>')
+    def number_route(n):
+        return f'Number: {n}'
 
     return app
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,4 +6,21 @@ def test_config():
 
 def test_index(client):
     response = client.get('/')
+    assert response.data == b'This is a flask boilerplate project, not to be used in production'
+
+def test_hello(client):
+    response = client.get('/hello')
     assert response.data == b'Hello World'
+
+    response = client.get('/hello?name=Jim')
+    assert response.data == b'Hello Jim'
+
+def test_number(client):
+    response = client.get('/number/1')
+    assert response.data == b'Number: 1'
+
+    response = client.get('/number/2')
+    assert response.data == b'Number: 2'
+
+    response = client.get('/number/apple')
+    assert response.status_code == 404
