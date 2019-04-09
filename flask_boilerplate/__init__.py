47c37dc435fc770135cd4023ac3dcf71e9a4d52c
Zane Waite <zmwaite182@stevenscollege.edu>
2019-04-08 21:04:38 -0400

    Add calculate route

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, render_template
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -14,19 +14,57 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        return 'This is a flask boilerplate project, not to be used in production'
+        return render_template('index.html')
 
     @app.route('/hello')
     def hello():
-
-        for key, value in request.args.items():
-            print(f"{key}: {value}")
-
         name = request.args.get('name', 'World')
-        return f'Hello {name}'
+        return render_template('hello.html', name=name)
 
     @app.route('/number/<int:n>')
     def number_route(n):
         return f'Number: {n}'
+
+
+    @app.route('/calculate', methods=['GET', 'POST'])
+    def calculate():
+        result = None
+        operator = '+'
+
+        if request.method == "GET":
+            return render_template('calculate.html', action="Add")
+        if request.method == "POST":
+            x = int(request.form['x'])
+            y = int(request.form['y'])
+            action = request.form['action']
+
+            if action == "Add":
+                result = x + y
+            if action == "Subtract":
+                operator = '-'
+                result = x - y
+            if action == "Multiply":
+                operator = '*'
+                result = x * y
+            if action == "Divide":
+                operator = '/'
+                result = x / y
+
+            return render_template("calculate.html", result=result, x=x, y=y, action=action, operator=operator)
+
+
+    method_route_allows = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+
+    @app.route('/method', methods=method_route_allows)
+    def method_route():
+        method = request.method
+        return render_template('methods.html', method=method, methods=method_route_allows)
+
+    @app.route('/status')
+    def status_route():
+        code = request.args.get('c', 200)
+        response = make_response("", code)
+        return response
+
 
     return app
--- tests/test_app.py
+++ tests/test_app.py
@@ -24,3 +24,30 @@ def test_number(client):
 
     response = client.get('/number/apple')
     assert response.status_code == 404
+
+def test_method_route(client):
+    response = client.get('/method')
+    assert response.data == b'HTTP Method: GET'
+
+    response = client.post('/method')
+    assert response.data == b'HTTP Method: POST'
+
+    response = client.patch('/method')
+    assert response.data == b'HTTP Method: PATCH'
+
+    response = client.put('/method')
+    assert response.data == b'HTTP Method: PUT'
+
+    response = client.delete('/method')
+    assert response.data == b'HTTP Method: DELETE'
+
+
+def test_status_route(client):
+    response = client.get('/status?c=400')
+    assert response.status_code == 400
+
+    response = client.get('/status?c=500')
+    assert response.status_code == 500
+
+    response = client.get('/status')
+    assert response.status_code == 200
